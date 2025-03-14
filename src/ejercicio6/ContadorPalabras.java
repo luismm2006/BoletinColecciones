f5798dadbb737e89c80a7369f40a1f7d5edca6c2
luismm2006 <luismulamarquez@gmail.com>
2025-03-15 00:47:08 +0100

    Subida ejercicio 6.

---
+++ src/ejercicio6/ContadorPalabras.java
@@ -0,0 +1,41 @@
+package ejercicio6;
+
+import java.util.*;
+
+class ContadorPalabras {
+    private String frase;
+
+    // Constructor
+    public ContadorPalabras(String frase) {
+        this.frase = frase;
+    }
+
+    public Set<String> obtenerPalabrasRepetidas() {
+        // pasamos a minúsculas y eliminamos distincion de acento
+        String fraseLimpia = frase.toLowerCase().replaceAll("[^a-záéíóúüñ ]", ""); 
+
+        
+        String[] palabras = fraseLimpia.split("\\s+|,"); 
+
+        Set<String> palabrasUnicas = new HashSet<>();
+        Set<String> palabrasRepetidas = new HashSet<>();
+
+        for (String palabra : palabras) {
+            if (!palabra.isEmpty()) { 
+                if (!palabrasUnicas.add(palabra)) { 
+                    palabrasRepetidas.add(palabra);
+                }
+            }
+        }
+
+        return palabrasRepetidas;
+    }
+
+
+    public static void main(String[] args) {
+        ContadorPalabras contador = new ContadorPalabras("Pues ná, no sí que me dijo no ni ná , sí.");
+        System.out.println("Palabras repetidas: " + contador.obtenerPalabrasRepetidas());
+    
+
+    }
+}
